692f448232b6ecda894a25f91ca78ebe5ff2ed22
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-14 13:20:12 +1200

    connected database with python

--- leagueoflegendsdb.py
+++ leagueoflegendsdb.py
@@ -1,1 +1,13 @@
-print("Hello world")
+import sqlite3
+
+
+DATABASE = "champions.db"
+
+db = sqlite3.connect(DATABASE)
+cursor = db.cursor()
+sql = "SELECT * FROM champions"
+cursor.execute(sql)
+results = cursor.fetchall()
+for champion in results:
+    print(champion)
+db.close()
